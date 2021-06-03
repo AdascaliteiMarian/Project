a7353f79ae6e9b5ae0a1adb23c5fa675ec099b6b
maryano1097 <maryano1096@gmial.com>
2021-06-03 15:30:52 +0300

    Navigation bar fixed

--- routes/Food.js
+++ routes/Food.js
@@ -7,7 +7,7 @@ var warning_not_admin;
 router.get('/', function(req, res){
     if(req.session.loggedin){
         USER_NAME = req.session.l_username;
-        res.render('Home_Page',{warning_not_admin});
+        res.render('Home_Page', {warning_not_admin});
     } else {
         res.set('Content-Type', 'text/plain')
     }
@@ -29,7 +29,7 @@ router.get('/', function(req, res){
 
  /* Add food to DB */
  var letters_instead='U wrote letters where they are not supposed to go';
- router.post('/add-food' ,function(req, res){
+ router.post('/add' , function(req, res){
     if(req.session.isadmin == true){
         let foodname = req.body.foodname;
         let kilocalories = req.body.kilocalories;
@@ -40,11 +40,11 @@ router.get('/', function(req, res){
         let comment = req.body.comment;
         if(foodname == '' || kilocalories == '' || water == '' || carbohydrates == '' || lipid == '' || proteins == ''){
             let warning_inputs = 'You have to fill every input'
-            res.render('Home_Page',{warning_inputs});
+            res.render('Home_Page', {warning_inputs});
         } else {
-            con.query('INSERT INTO foods(foodname,kilocalories, proteins,lipid,carbohydrates,water,comment) VALUES(?,?,?,?,?,?,?)',[foodname,kilocalories,proteins,lipid,carbohydrates,water,comment], function(err, result){
+            con.query('INSERT INTO foods(foodname, kilocalories, proteins, lipid, carbohydrates, water, comment) VALUES(?, ?, ?, ?, ?, ?, ?)', [foodname, kilocalories, proteins, lipid, carbohydrates, water, comment], function(err, result){
                 if(err) {
-                    res.render('Home_Page',{letters_instead});
+                    res.render('Home_Page', {letters_instead});
                 }
             })
         }
@@ -56,25 +56,25 @@ router.get('/', function(req, res){
  })
 
 /* Sort Food List by Alphabetical Order */
- router.get('/sort-alpha',function(req, res){
+ router.get('/sort-alpha', function(req, res){
      con.query('SELECT * FROM foods ORDER BY foodname', function(err, result){
-         res.render('food_list',{result});
+         res.render('food_list', {result});
          console.log("List sorted Alphabetically");
      })
  })
 
 /* Sort Food List by number of least Calories*/
- router.get('/sort-by-least-calories',function(req, res){
+ router.get('/sort-by-least-calories', function(req, res){
     con.query('SELECT * FROM foods ORDER BY kilocalories', function(err, result){
-        res.render('food_list',{result});
+        res.render('food_list', {result});
         console.log("List sorted by number of calories");
     })
 })
 
 /* Sort Food List by number of most Calories*/
-router.get('/sort-by-most-calories',function(req, res){
+router.get('/sort-by-most-calories', function(req, res){
     con.query('SELECT * FROM foods ORDER BY kilocalories DESC', function(err, result){
-        res.render('food_list',{result});
+        res.render('food_list', {result});
         console.log("List sorted by number of calories");
     })
 })
